f4779eabd2c38b936f75695229684378253af68a
anli <wufeii1999@gmail.com>
2023-09-20 22:30:23 -0400

    data transformation stage done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.TextSummarizer.logging import logger
 from TextSummarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.pipeline.data_validation import DataValidationTrainingPipeline
+from TextSummarizer.pipeline.data_transformation import DataTransformationTrainingPipeline
 
 logger.info("Welcome to your logging")
 
@@ -19,6 +20,16 @@ try:
     logger.info(f">>>>>>> Starting {STAGE_NAME} <<<<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>> Finished {STAGE_NAME} <<<<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "data_transformation"
+try:
+    logger.info(f">>>>>>> Starting {STAGE_NAME} <<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>> Finished {STAGE_NAME} <<<<<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
--- src/TextSummarizer/components/data_ingestion.py
+++ src/TextSummarizer/components/data_ingestion.py
@@ -10,7 +10,6 @@ class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
     
-    
     def download_file(self):
         if not os.path.exists(self.config.local_data_dir):
             filename, headers = request.urlretrieve(
@@ -20,8 +19,7 @@ class DataIngestion:
             logger.info(f"{filename} download! with following info: \n{headers}")
         else:
             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_dir))}")  
-
-        
+    
     def extract_zip_file(self):
         """
         zip_file_path: str
